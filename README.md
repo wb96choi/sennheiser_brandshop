0b44bb166998444cbcce7437121072c7b0793a33
1_b1n <choi44601280@gmail.com>
2022-07-11 12:46:18 +0900

    데이터입력

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # sennheiser-mobile
 mobile shop homepage
 https://wb96choi.github.io/sennheiser_brandshop/
+
+
+1. 가변 타이틀 UI
+2. GNB toggle & animation
+3. 검색창 열고닫기
+4. 제품보기 - data fetching
+5. 카테고리 메뉴
+6. 상세페이지
--- js/app.js
+++ js/app.js
@@ -1,112 +1,271 @@
-// app.js----------------------
-
-$(function(){
-
-    /* #gnb toggle
-        1. 열기: #toggle-btn 클릭시 #gnb on
-        2. 닫기: #btn-close 클릭시 #gnb
-    */
-    $('#toggle-btn').click(function(){
-        $('#gnb').addClass('on');
-    });
-    $('#btn-close').click(function(){
-        $('#gnb').removeClass('on');
-    });
-
-
-    // 검색창 열고닫기 ----------------
-    $('#icon-menu .bi-search').click(function(){
-        $(".search-box").addClass('on');
-    });
-    $('.search-box #btn-close').click(function(){
-        $(".search-box").removeClass('on');
-    });
-
-
-
-});
-
-
-
-
-// 제품 데이터 가져오기
-function getData() {
-    fetch('https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.json')
-    .then(function(res){
-        return res.json(); // JSON 객체 변환
-    })                
-    .then(function(obj){
-        // obj 동물데이터
-        showanimals(obj);    
-    });
-};
-
-
-
-function showanimals(obj) {
-// URL parameter(매개변수)
-const query = location.search;
-console.log(query);
-// ? URL query문을 object(변수)로 변경
-let params = new URLSearchParams(query).get('category');
-console.log(params);
-
-// params == null 이면 시작 페이지 dog 출력
-if(params == null) {
-    params = 'dog'
-}
-
-
-/*
-    파라미터를 전달하여 요청하기
-    홈페이지주소name = 홍길동
-        매개변수(URL parameter) name = '홍길동'
-    index.html -> index.html?category=dog -> 
-    강아지 보여주세요
-    cat.html -> cat.html?category=cat ->
-    고양이 보여주세요
-    bird.html -> bird.html?category=bird ->
-    새 보여주세요
-*/ 
-
-// forEach() 반복문 = if반복문보다 간단함
-obj.forEach(function(animal){
-    // 카테고리 구분 dog | cat | bird
-    // 요청한 params와 동물카테고리명이 일치하는 데이터만 출력 
-    if(params == animal.category) {
-        // console.log('params = ', params);
-        // console.log(animal.category);
-
-        let html = `
-        <div class="col">
-            <img src=${animal.imgUrl} alt="dog01">
-            <p class="name">${animal.name}</p>
-        </div>    
-    `
-    $('.row').append(html);
-    };
-    
-});
-};
-
+// 검색창 컴포넌트 UI
 $(function(){
     const html = `
-    <!-- 검색 UI----------------- -->
-    <div class="search-box">
+      <!-- 검색 UI -->
+      <div class="search-box">
         <form class="search-box-container">
-            <input 
-                type="search" 
-                id="search"
-                placeholder="검색">
-            <div class="button-group">
-                <button id="submit" type="submit">
-                    <i class="bi bi-search"></i>
-                </button>
-                <button id="btn-close">
-                    <i class="bi bi-x-lg"></i>
-                </button>
-            </div>
+          <input 
+            type="search" 
+            id="search"
+            placeholder="검색"
+            required
+          >
+          <div class="button-group">
+            <button id="submit" type="submit">
+              <i class="bi bi-search"></i>
+            </button>
+            <button class="btn-close">
+              <i class="bi bi-x-lg"></i>
+            </button>
+          </div>
         </form>
-    </div>`;
-    $('body').append(html);
-});
+      </div>`;
+    $('body').append(html); 
+  
+  }); // $
+  
+  
+  $(function(){
+  
+    /*** #gnb toggle ***/
+    // 1. 열기: #toggle-btn 클릭시 #gnb on
+    $('#toggle-btn').click(function(){
+      $('#gnb').addClass('on');
+    });
+    // 2. 닫기: #btn-close 클릭시 #gnb 닫음
+    $('#btn-close').click(function(){
+      $('#gnb').removeClass('on');
+    });
+  
+    /*** 검색창 열고, 닫기 */
+    // 1. #icon-menu .bi-search 누르면 열기
+    $('#icon-menu .bi-search').click(function(){
+      $(".search-box").addClass('on');
+    });
+    // 2. .search-box .btn-close 누르면 닫기
+    $('.search-box #btn-close').click(function(){
+      $(".search-box").removeClass('on');
+    })
+  }); // $
+  
+  
+  
+  // 제품 데이터 가져오기
+  function getData() {
+    // 여기에 여러분 깃허브 JSON 파일 경로(서버 데이터 주소)
+    const DataURL = 'https://raw.githubusercontent.com/csslick/sennheiser-mobile/main/data.json';
+    fetch(DataURL)
+    .then(function(res){
+      return res.json(); // JSON 객체 변환
+    })
+    .then(function(obj){
+      // obj 동물데이터
+      showProducts(obj);
+      console.log(obj);
+    });
+  }
+  
+  function showProducts(obj) {
+    // 현재 페이지의 URL query 파라미터(매개변수)
+    const query = location.search;
+    console.log(query);
+    // ? URL query문을 object(변수)로 변경
+    let params = new URLSearchParams(query).get('category');
+  
+    // params == null 이면(시작 페이지 모든 제품 출력)
+    if(params == null || params =='all') {
+      params = null;
+    }
+    console.log(params);  
+  
+    // 동물 데이터 출력
+    obj.forEach(function(product){
+      // 카테고리 구분 wireless | 무선헤드폰 | 유선헤드폰
+      // 요청한 params와 제품카테고리명이 일치하는 데이터만 출력
+      let category = product.category;
+      let name = product.name;
+      let price = product.price;
+      let imgUrl = product.imgUrl;
+      let text = product.text;
+      console.log(category);
+      if(params == product.category) {
+        let html = `
+        <div class="product">
+          <a href="detail.html">
+            <img src="${imgUrl}" alt="">
+            <div class="info">
+              <span class="category">${category}</span>
+              <p class="title">${name}</p>
+              <p class="price"><span>₩</span>${price}</p>
+            </div>
+          </a>
+        </div>
+        `
+      $('.row').append(html);
+      }
+  
+    });
+  }
+  
+  $(function(){
+    getData();
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 검색창 컴포넌트 UI
+// $(function(){
+//   const html = `
+//     <!-- 검색 UI -->
+//     <div class="search-box">
+//       <form class="search-box-container">
+//         <input 
+//           type="search" 
+//           id="search"
+//           placeholder="검색"
+//           required
+//         >
+//         <div class="button-group">
+//           <button id="submit" type="submit">
+//             <i class="bi bi-search"></i>
+//           </button>
+//           <button class="btn-close">
+//             <i class="bi bi-x-lg"></i>
+//           </button>
+//         </div>
+//       </form>
+//     </div>`;
+//   $('body').append(html); 
+
+// }); // $
+
+
+// $(function(){
+
+//   /*** #gnb toggle ***/
+//   // 1. 열기: #toggle-btn 클릭시 #gnb on
+//   $('#toggle-btn').click(function(){
+//     $('#gnb').addClass('on');
+//   });
+//   // 2. 닫기: #btn-close 클릭시 #gnb 닫음
+//   $('#btn-close').click(function(){
+//     $('#gnb').removeClass('on');
+//   });
+
+//   /*** 검색창 열고, 닫기 */
+//   // 1. #icon-menu .bi-search 누르면 열기
+//   $('#icon-menu .bi-search').click(function(){
+//     $(".search-box").addClass('on');
+//   });
+//   // 2. .search-box .btn-close 누르면 닫기
+//   $('.search-box .btn-close').click(function(){
+//     $(".search-box").removeClass('on');
+//   })
+// }); // $
+
+
+
+// // 제품 데이터 가져오기
+// function getData() {
+//   // 여기에 여러분 깃허브 JSON 파일 경로(서버 데이터 주소)
+//   const DataURL = 'https://raw.githubusercontent.com/csslick/sennheiser-mobile/main/data.json';
+//   fetch(DataURL)
+//   .then(function(res){
+//     return res.json(); // JSON 객체 변환
+//   })
+//   .then(function(obj){
+//     // obj 동물데이터
+//     showProducts(obj);
+//     console.log(obj);
+//   });
+// }
+
+// function showProducts(obj) {
+//   // 현재 페이지의 URL query 파라미터(매개변수)
+//   const query = location.search;
+//   console.log(query);
+//   // ? URL query문을 object(변수)로 변경
+//   let params = new URLSearchParams(query).get('category');
+
+//   // params == null 이면(시작 페이지 dog 출력)
+//   if(params == null) {
+//     params = 'wireless'
+//   }
+//   console.log(params);  
+
+//   // 동물 데이터 출력
+//   obj.forEach(function(product){
+//     // 카테고리 구분 wireless | 무선헤드폰 | 유선헤드폰
+//     // 요청한 params와 제품카테고리명이 일치하는 데이터만 출력
+//     let category = product.category;
+//     console.log(category);
+//     if(params == product.category) {
+//       let html = `
+//       <div class="product">
+//         <a href="detail.html">
+//           <img src="images/cx plus true wireless k.webp" alt="">
+//           <div class="info">
+//             <span class="category">${category}</span>
+//             <p class="title">MOMEN TUM True Wireless 3</p>
+//             <p class="price"><span>₩</span>170,900</p>
+//           </div>
+//         </a>
+//       </div>
+//       `
+//     $('.row').append(html);
+//     }
+
+//   });
+// }
+
+// $(function(){
+//   getData();
+// })
